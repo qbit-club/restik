0cc02927833363cb5e49cad82300e824ad1b5b98
qbclub <17926057+qbclub@users.noreply.github.com>
2024-08-27 22:20:39 +0400

    small ui fix

--- components/restindex/InfoCard.vue
+++ components/restindex/InfoCard.vue
@@ -8,9 +8,9 @@ defineProps<{
 </script>
 <template>
   <v-container>
-    <v-row class="justify-center pb-16">
+    <v-row class="justify-center pb-16 pt-8">
       <v-col :cols="12" :sm="10" :md="8" class="pa-0">
-        <v-card class="pa-8" width="100%" min-width="300" title="Информация">
+        <v-card class="pa-4" width="100%">
         <p>
           <span v-html="rest.description"></span>
         </p>
