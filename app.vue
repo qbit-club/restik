e72cb38305061c90eb2ef0bdb128bbcf12a4ac7f
qbclub <17926057+qbclub@users.noreply.github.com>
2025-05-09 06:59:51 +0400

    add icons

--- app.vue
+++ app.vue
@@ -2,8 +2,34 @@
 useHead({
   title: "Глазов-есть!",
   meta: [
-      { name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' }
-    ]
+    { name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' }
+  ],
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '192x192',
+      href: '/android-chrome-192x192.png',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '512x512',
+      href: '/android-chrome-512x512.png',
+    },
+    {
+      rel: 'apple-touch-icon',
+      type: 'image/png',
+      sizes: '180x180',
+      href: '/apple-touch-icon.png',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '512x512',
+      href: '/pwa-512x512.png',
+    },
+  ],
 })
 </script>
 <template>
